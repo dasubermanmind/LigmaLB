f460b9d10738fa663f9a6f37ad3c8291f6505e7c
dasubermanmind <ero33@drexel.edu>
2022-11-21 20:04:12 -0500

    WIP

--- ligma_load_balancer.py
+++ ligma_load_balancer.py
@@ -1,8 +1,13 @@
-from flask import Flask
-
+from flask import Flask, request
+import random 
+import requests
 
 ligma_load_balancer = Flask(__name__)
 
 @ligma_load_balancer.route('/')
 def router():
-    return 'Welcome to Ligma'
+    host_header = request.headers['Host']
+    if host_header == 'test':
+        # THis is for testing and not for an actual impl
+        response = requests.get('')
+
